12cded8787c3fc2a8b6c357d5485db080d871738
mianibrahim1 <mianibrahim79@gmail.com>
2017-09-14 14:31:10 -0400

    Improvement to the None case

--- binary_search.py
+++ binary_search.py
@@ -6,22 +6,24 @@ class binary_search:
         pass
     def find_number(self, array, find_num):
         self.first = 0
-        self.last = len(array) - 1
+        self.last = len(array)
         self.not_found = False
         while not self.not_found:
             self.middle = int((self.last + self.first)/2)
+            print self.first
             if array[self.middle] == find_num:
                 return self.middle
             elif array[self.middle] < find_num:
                 self.first = self.middle
             elif array[self.middle] > find_num:
                 self.last = self.middle
-            elif self.first == self.middle and self.middle == self.last:
+
+            if self.last == 0 or self.first == len(array) - 1:
                 self.not_found = True
 
 
 #basic test cases
 new_class = binary_search()
-print new_class.find_number([1,2,3,4],3)
+print new_class.find_number([1,2,3,4],5)
 print new_class.find_number([1,2,3],2)
 print new_class.find_number([1,2,3],1)
